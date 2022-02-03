7b2ea34292ace22fc5439f3ab750654e036d64bf
Artem Pyvovar <artefan40@gmail.com>
2022-02-03 18:13:34 +0200

    chore: setup store

--- src/epic/get-started.ts
+++ src/epic/get-started.ts
@@ -1,13 +1,15 @@
 import TelegramBot, { Message } from 'node-telegram-bot-api';
 import BaseCommands from '../commands/base';
+import store from '../store';
 
 function getStarted(bot: TelegramBot) {
+  const lang = store.getState().lang;
   bot.on('message', async (message: Message) => {
     const text = message.text;
     const chatId = message.chat.id;
 
     if (text === BaseCommands['/start']) {
-      await bot.sendMessage(chatId, 'Добро пожаловать мазеФАКЕР');
+      await bot.sendMessage(chatId, `Добро пожаловать ${lang}`);
     }
   });
 }
